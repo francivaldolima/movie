ede78243ede57768edb1c10b67b7a12755dd7780
francivaldolima <francivaldolimadesousa@gmail.com>
2022-05-04 15:35:18 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,20 +1,24 @@
+import { type } from "@testing-library/user-event/dist/type";
 import MovieStars from "components/MovieStars";
-import './styles.css';
+import "./styles.css";
+
+type Props = {
+  score: number;
+  count: number;
+
+}
 
 
-function MovieScore() {
-
-  const score = 3.5;
-  const count = 13;
-
+function MovieScore({score, count} : Props ) {
   return (
     <div className="dsmovie-score-container">
-      <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : "-"}</p>
-      < MovieStars/>
+      <p className="dsmovie-score-value">
+        {score > 0 ? score.toFixed(1) : "-"}
+      </p>
+      <MovieStars score={score} />
       <p className="dsmovie-score-count">{count} avaliações</p>
     </div>
   );
 }
 
 export default MovieScore;
-
